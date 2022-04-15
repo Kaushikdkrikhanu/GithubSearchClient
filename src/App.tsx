435107a6338f1f201a2207d3a7f40d04b86a2a7b
kaushik12dkrikhanu <kaushik12dkrikhanu@gmail.com>
2022-04-16 03:41:06 +0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react';
+import axios from 'axios';
 import './App.css';
+import debounce from 'lodash.debounce';
 
-function App() {
+const baseURL= 'http://githubusersearch-env.eba-rtqyhpjt.us-west-1.elasticbeanstalk.com'
+const API = axios.create({baseURL: baseURL})
+interface User{
+  name: string,
+  url: string,
+  avatar: string,
+}
+
+const App: React.FC = ()=> {
+  const [users,setUsers] = useState<User []>([]);
+  const onChangeHandler = async (e:any)=>{    
+     debounceCalls(e);   
+  }
+  const debounceCalls = debounce(async (e)=>{
+    if(e.target.value.length<3){
+      setUsers([])
+    }
+    else{
+      console.log(e.target.value);
+      const result = await API.post('/api/search',{text: e.target.value, type: "users"})
+      const newUSers: User[] = result.data
+      setUsers(newUSers);
+      console.log(result);
+    }
+    
+  },300);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>      
+      <div className='navbar'>
+      <input placeholder='Search' onChange={onChangeHandler} className="button" /> 
+      </div>
+      <div className='cardContainer'>{users.length>0?users.map((item)=>(
+        <div key={item.url} className="card">
+          <a href={`https://github.com/${item.name}`} className="link"><img alt='avatar' className='userAvatar' src={item.avatar}/></a>
+          <a href={`https://github.com/${item.name}`} className="link"><div className='userName'>{item.name}</div></a>
+          </div>
+      )):null}</div>
     </div>
   );
 }
